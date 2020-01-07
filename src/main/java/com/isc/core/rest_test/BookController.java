5554bb2f71ad879232fda4a0c2db1f64000a022e
A_Chehreghani <A_Chehreghani@ISC.CO.IR>
2020-01-07 13:43:42 +0330

    second

---
+++ src/main/java/com/isc/core/rest_test/BookController.java
@@ -0,0 +1,17 @@
+package com.isc.core.rest_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BookController {
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        return Arrays.asList(new Book());
+    }
+
+}
