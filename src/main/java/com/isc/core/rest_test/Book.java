d0ad99e023154c605f0bdede2299313bb8c6c446
A_Chehreghani <A_Chehreghani@ISC.CO.IR>
2020-01-07 13:43:07 +0330

    second

---
+++ src/main/java/com/isc/core/rest_test/Book.java
@@ -0,0 +1,31 @@
+package com.isc.core.rest_test;
+
+public class Book {
+    Long ID;
+    String name;
+    String authorName;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
